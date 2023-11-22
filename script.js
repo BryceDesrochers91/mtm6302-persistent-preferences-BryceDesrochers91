24d02bd3ca52b5c01a8a6dc034e7b6c9ba23caa3
Bryce Desrochers <desr0151@algonquinlive.com>
2023-11-22 09:15:17 -0500

    adds copy over from week 11 in class assignment to persisitent preferences assignment.

---
+++ script.js
@@ -0,0 +1,49 @@
+$list = document.getElementById('list')
+$theme = document.getElementById('theme')
+$preferencesPanel = document.getElementById('preferencesPanel')
+
+
+
+let preferences = [
+    'Buy Dinner',
+    'Completes assignment',
+    'review class'
+
+]
+
+function buildToDoList(){
+
+const html = []
+
+for( let i=0; i < tasks.length; i++){
+    html.push(`<li class="list-group-item">${tasks[i]}<button class="btn btn-danger">Delete</button></li>`)
+}
+    $list.innerHTML = html.join('')
+
+}
+
+// buildToDoList()
+
+$form.addEventListener('submit', function(e){
+    e.preventDefault()
+
+    tasks.push($newTask.value)
+    $newTask.value = ''
+    buildToDoList()
+})
+
+$list.addEventListener('click', function(e){
+    console.log(e.target)
+
+    if (e.target.classList.contains('btn')) {
+        
+    }
+    tasks.splice(e.target.dataset.index, 1)
+    buildToDoList()
+    localStorage.setItem('tasks', JSON.stringify(tasks))
+})
+
+if(localStorage.getItem('tasks')){
+    tasks = JSON.parse(localStorage.getItem(tasks))
+}
+buildToDoList()
